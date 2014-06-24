871cf58e6f5a925739db0082a4c5200d5d0128d5
unknown <amaia@amaia.compugraf.com.br>
2014-06-24 18:28:42 -0300

    0.1.0 version of the projeto

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ShowMeTheBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * 0) quit
+             * 1) int
+             * 2) double
+             * 3) uint
+             * 4) short
+             */
+
+            var option = 0;
+
+            Console.WriteLine("ShowMeTheBits");
+            Console.WriteLine("0) quit");
+            Console.WriteLine("1) int");
+            Console.WriteLine("2) double");
+            Console.WriteLine("3) uint");
+            Console.WriteLine("4) short");
+
+
+
+            do
+            {
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        int i = 8;
+                        var bytes = BitConverter.GetBytes(i);
+                        printBits(bytes);
+                        break;
+
+                    case 2:
+                        double d = 8d;
+                        printBits(BitConverter.GetBytes(d));
+                        break;
+
+                    case 3:
+                        uint ui = 8;
+                        printBits(BitConverter.GetBytes(ui));
+                        break;
+
+                    case 4:
+                        short s = 8;
+                        printBits(BitConverter.GetBytes(s));
+                        break;
+
+                    default:
+                        Console.WriteLine("See you son!");
+                        break;
+                }
+
+                Console.WriteLine(string.Empty);
+            } while (option != 0);
+        }
+
+        private static void printBits(byte[] bytes)
+        {
+            var str = new List<string>();
+            var bits = new BitArray(bytes);
+
+            for (int i = 0; i < bits.Length; i++)
+            {
+                var bit = bits[i];
+                if (bit)
+                    str.Add("1");
+                else
+                    str.Add("0");
+
+                // separator
+                if ((i+1) % 4 == 0)
+                    str.Add(" ");
+            }
+
+            // print the bits list in string.
+            Console.WriteLine(string.Join("", string.Join("", str).Reverse()));
+        }
+    }
+}
